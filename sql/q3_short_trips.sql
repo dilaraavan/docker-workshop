c866f2a438cdc59ec5979253feca14ebb75d6ba2
Dilara AVAN <dilaraavan@gmail.com>
2026-01-29 22:41:03 +0300

    Update Q3 to use green_taxi table

--- sql/q3_short_trips.sql
+++ sql/q3_short_trips.sql
@@ -1,6 +1,6 @@
 -- Q3: Counting short trips (<= 1 mile) in November 2025
 SELECT COUNT(*)
-FROM green_tripdata
+FROM green_taxi
 WHERE
   lpep_pickup_datetime >= '2025-11-01'
   AND lpep_pickup_datetime < '2025-12-01'
