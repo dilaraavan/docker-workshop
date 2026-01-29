1e363185d9bf19612016746c98e08ec8e39075ab
Dilara AVAN <dilaraavan@gmail.com>
2026-01-29 22:19:11 +0300

    Add SQL query to count short trips in November 2025
    This query counts the number of short trips (<= 1 mile) taken in November 2025.

---
+++ sql/q3_short_trips.sql
@@ -0,0 +1,7 @@
+-- Q3: Counting short trips (<= 1 mile) in November 2025
+SELECT COUNT(*)
+FROM green_tripdata
+WHERE
+  lpep_pickup_datetime >= '2025-11-01'
+  AND lpep_pickup_datetime < '2025-12-01'
+  AND trip_distance <= 1;
